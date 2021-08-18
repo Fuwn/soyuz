9cb81924b3f7e7bc441bb80d0aba9aefe61f2674
Fuwn <fuwnzy@gmail.com>
2021-08-18 02:33:39 -0700

    feat(soyuz): :rocket:

---
+++ include/soyuz/soyuz.hh
@@ -0,0 +1,12 @@
+// Copyright (C) 2021-2021 Fuwn
+// SPDX-License-Identifier: GPL-3.0-only
+
+#ifndef SOYUZ_SOYUZ_HH
+#define SOYUZ_SOYUZ_HH
+
+#pragma once
+
+#define LUNAR_WINDOW_NAME_BASE "Lunar Client ("
+#define DISCORD_IPC_NAMED_PIPE_NAME L"\\Device\\NamedPipe\\discord-ipc-0"
+
+#endif //SOYUZ_SOYUZ_HH
