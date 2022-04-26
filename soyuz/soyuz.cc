11af004d24556f8510fbee70664469e4ec197cb0
Fuwn <contact@fuwn.me>
2022-04-26 21:15:07 +0000

    refactor(soyuz): global style refactoring

--- soyuz/soyuz.cc
+++ soyuz/soyuz.cc
@@ -7,8 +7,6 @@
  * @date   2021. August. 18.
  */
 
-#include <soyuz/soyuz.hh>
-
 #pragma comment(lib, "ntdll.lib")
 
 #include <fmt/format.h>
@@ -39,7 +37,6 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int show) {
   wincl.lpfnWndProc = WindowProcedure;
   wincl.style = CS_DBLCLKS;
   wincl.cbSize = sizeof(WNDCLASSEX);
-
   wincl.hIcon = LoadIcon(GetModuleHandle(nullptr), MAKEINTRESOURCE(ICO1));
   wincl.hIconSm = LoadIcon(GetModuleHandle(nullptr), MAKEINTRESOURCE(ICO1));
   wincl.hCursor = LoadCursor(nullptr, IDC_ARROW);
@@ -47,6 +44,7 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int show) {
   wincl.cbClsExtra = 0;
   wincl.cbWndExtra = 0;
   wincl.hbrBackground = (HBRUSH)(CreateSolidBrush(RGB(255, 255, 255)));
+
   if (!RegisterClassEx(&wincl)) { return 0; }
 
   window = CreateWindowEx(
@@ -63,6 +61,7 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int show) {
     instance,
     nullptr
   );
+
   InitNotifyIconData();
   ShowWindow(window, show);
 
@@ -74,18 +73,45 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int show) {
   std::jthread soyuz {[](const std::stop_token &stop) -> void {
     // Check if Lunar Client is open, if not; close Soyuz
     DWORD pid = soyuz::find_lunar();
+
     if (pid == 0 || pid == 3435973836) {
-      soyuz::log(soyuz::log_level::LOG_LEVEL_ERROR, "could not locate lunar client");
-      soyuz::log(soyuz::log_level::LOG_LEVEL_WARN, "this window will close in five seconds");
+      soyuz::log(
+        soyuz::log_level::LOG_LEVEL_ERROR,
+        "could not locate lunar client"
+      );
+      soyuz::log(
+        soyuz::log_level::LOG_LEVEL_WARN,
+        "this window will close in five seconds"
+      );
+
       for (int i = 4; i > -1; --i) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        soyuz::log(soyuz::log_level::LOG_LEVEL_WARN, fmt::format("> {} second{}", soyuz::numbers_as_string[i], i != 1 ? "s" : ""));
+
+        soyuz::log(
+          soyuz::log_level::LOG_LEVEL_WARN,
+          fmt::format(
+            "> {} second{}",
+            soyuz::numbers_as_string[i],
+            i != 1 ? "s" : ""
+          )
+        );
       }
 
       soyuz::exit(1);
     }
-    soyuz::log(soyuz::log_level::LOG_LEVEL_INFO, fmt::format("located lunar client: pid {}", pid)); // GetLastError()
-    soyuz::log(soyuz::log_level::LOG_LEVEL_INFO, "hooked lunar client"); soyuz::log(soyuz::log_level::LOG_LEVEL_INFO, "you may now close this window");
+
+    soyuz::log(
+      soyuz::log_level::LOG_LEVEL_INFO,
+      fmt::format(
+        "located lunar client: pid {}",
+        pid
+      )
+    ); // GetLastError()
+    soyuz::log(soyuz::log_level::LOG_LEVEL_INFO, "hooked lunar client");
+    soyuz::log(
+      soyuz::log_level::LOG_LEVEL_INFO,
+      "you may now close this window"
+    );
 
     while (!stop.stop_requested()) {
       /**
@@ -94,14 +120,22 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int show) {
        * Thanks, @LorenzoHanssens (#1)
        */
       pid = soyuz::find_lunar();
+
       if (pid == 0 || pid == 3435973836) {
-        soyuz::log(soyuz::log_level::LOG_LEVEL_WARN, "could not locate lunar client, waiting 10 seconds");
+        soyuz::log(
+          soyuz::log_level::LOG_LEVEL_WARN,
+          "could not locate lunar client, waiting 10 seconds"
+        );
+
         std::this_thread::sleep_for(std::chrono::seconds(10));
       }
 
       // If Lunar Client **is** open, close it's Discord IPC Named Pipe
       if (soyuz::delete_handle(pid) == 1) {
-        soyuz::log(soyuz::log_level::LOG_LEVEL_WARN, "unable to close lunar client's discord ipc named pipe");
+        soyuz::log(
+          soyuz::log_level::LOG_LEVEL_WARN,
+          "unable to close lunar client's discord ipc named pipe"
+        );
       }
     }
   }};
