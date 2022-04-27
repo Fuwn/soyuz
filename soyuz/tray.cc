5220f9de2710902768630d5521f36b8b90e1b664
Fuwn <contact@fuwn.me>
2022-04-26 21:15:07 -0700

    refactor(soyuz): global style refactoring

--- soyuz/tray.cc
+++ soyuz/tray.cc
@@ -16,68 +16,75 @@
 #include <soyuz/resource.hh>
 #include <soyuz/soyuz.hh>
 
-UINT WM_TASKBAR =   0;
+UINT WM_TASKBAR = 0;
 HWND window;
 HMENU menu;
 NOTIFYICONDATA data;
-TCHAR tip[64] =     TEXT(WINDOW_TRAY_NAME);
+TCHAR tip[64] = TEXT(WINDOW_TRAY_NAME);
 char class_name[] = WINDOW_TRAY_NAME;
 std::vector<soyuz::log_t> logs;
 
-LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+auto CALLBACK WindowProcedure(
+  HWND   hwnd,
+  UINT   message,
+  WPARAM wParam,
+  LPARAM lParam
+) -> LRESULT {
   if (message == WM_TASKBAR && !IsWindowVisible(window)) {
-    minimize(); return 0;
+    minimize();
+
+    return 0;
   }
 
   switch (message) {
-    case WM_ACTIVATE:
-      Shell_NotifyIcon(NIM_ADD, &data);
-      break;
+    case WM_ACTIVATE: { Shell_NotifyIcon(NIM_ADD, &data); } break;
 
     case WM_PAINT: {
       PAINTSTRUCT ps;
       RECT rect;
       HDC hdc = BeginPaint(window, &ps);
+      int height = 5;
 
       GetClientRect(hwnd, &rect);
 
-      int height = 5;
-      for (soyuz::log_t &i : logs) {
-        SetTextColor(hdc, i.to_colorref());
-        TextOut(hdc, 5, height, i.value.c_str(), (int)strlen(i.value.c_str()));
+      for (soyuz::log_t &log : logs) {
+        SetTextColor(hdc, log.to_colorref());
+        TextOut(
+          hdc,
+          5,
+          height,
+          log.value.c_str(),
+          (int)strlen(log.value.c_str())
+        );
+
         height += 20;
       }
 
       EndPaint(window, &ps);
     } break;
 
-    case WM_CREATE:
+    case WM_CREATE: {
       ShowWindow(window, SW_HIDE);
-      menu = CreatePopupMenu();
-      AppendMenu(menu, MF_STRING, ID_TRAY_EXIT, TEXT("Exit Soyuz"));
 
-      break;
+      menu = CreatePopupMenu();
+
+      AppendMenu(menu, MF_STRING, ID_TRAY_EXIT, TEXT("Exit Soyuz"));
+    } break;
 
     case WM_SYSCOMMAND:
       switch(wParam & 0xFFF0) {
         case SC_MINIMIZE:
-        case SC_CLOSE:
-          minimize(); return 0; // break;
+        case SC_CLOSE: { minimize(); return 0; } // break;
       } break;
 
     case WM_SYSICON: {
-      switch (wParam) {
-        case ID_TRAY_APP_ICON:
-          SetForegroundWindow(window);
-          break;
-
-        default: ;
-      }
+      if (wParam == ID_TRAY_APP_ICON) { SetForegroundWindow(window); }
 
       if (lParam == WM_LBUTTONUP) {
         restore();
       } else if (lParam == WM_RBUTTONDOWN) {
         POINT curPoint;
+
         GetCursorPos(&curPoint);
         SetForegroundWindow(window);
 
@@ -92,6 +99,7 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
         );
 
         SendMessage(hwnd, WM_NULL, 0, 0);
+
         if (clicked == ID_TRAY_EXIT) {
           Shell_NotifyIcon(NIM_DELETE, &data);
           PostQuitMessage(0);
@@ -101,6 +109,7 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
 
     case WM_NCHITTEST: {
       LRESULT uHitTest = DefWindowProc(hwnd, WM_NCHITTEST, wParam, lParam);
+
       if (uHitTest == HTCLIENT) {
         return HTCAPTION;
       } else {
@@ -108,13 +117,9 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
       }
     }
 
-    case WM_CLOSE:
-      minimize(); return 0; // break;
-
-    case WM_DESTROY:
-      PostQuitMessage (0); break;
-
-    default: ;
+    case WM_CLOSE: { minimize(); return 0; } // break;
+    case WM_DESTROY: { PostQuitMessage(0); } break;
+    default: {} break;
   }
 
   return DefWindowProc(hwnd, message, wParam, lParam);
@@ -132,17 +137,19 @@ void InitNotifyIconData() {
   data.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
   data.uCallbackMessage = WM_SYSICON;
   data.hIcon = (HICON)LoadIcon(GetModuleHandle(nullptr), MAKEINTRESOURCE(ICO1));
+
   strncpy_s(data.szTip, tip, sizeof(tip));
 }
 
 namespace soyuz {
 
-auto log(const std::string &message) -> void {
+[[maybe_unused]] auto log(const std::string &message) -> void {
   if (logs.size() == 16) { logs.erase(logs.begin()); }
 
   std::string to_log = fmt::format("[{}] {}", current_date_time(), message);
 
-  logs.emplace_back(log_t(log_level::LOG_LEVEL_INFO, to_log)); write_log_file(to_log);
+  logs.emplace_back(log_t(log_level::LOG_LEVEL_INFO, to_log));
+  write_log_file(to_log);
   RedrawWindow(window, nullptr, nullptr, RDW_INVALIDATE | RDW_UPDATENOW);
 }
 
@@ -151,7 +158,8 @@ auto log(log_level level, const std::string &message) -> void {
 
   std::string to_log = fmt::format("[{}] {}", current_date_time(), message);
 
-  logs.emplace_back(log_t(level, to_log)); write_log_file(to_log);
+  logs.emplace_back(log_t(level, to_log));
+  write_log_file(to_log);
   RedrawWindow(window, nullptr, nullptr, RDW_INVALIDATE | RDW_UPDATENOW);
 }
 
