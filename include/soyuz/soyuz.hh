8d988095968e00ddaf6963f94de7d5f89556a82c
Fuwn <contact@fuwn.me>
2023-05-06 02:26:32 +0000

    docs: bump copyright headers

--- include/soyuz/soyuz.hh
+++ include/soyuz/soyuz.hh
@@ -1,11 +1,5 @@
-// Copyright (C) 2021-2022 Fuwn
+// Copyright (C) 2021-2023 Fuwn
 // SPDX-License-Identifier: GPL-3.0-only
-
-/**
- * @file   soyuz.hh
- * @author Fuwn
- * @date   2021. August. 18.
- */
 
 #ifndef SOYUZ_SOYUZ_HH
 #define SOYUZ_SOYUZ_HH
