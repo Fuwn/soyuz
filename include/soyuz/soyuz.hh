218a33454ca7a2d0af4cf23300909d7364ee18d0
Fuwn <contact@fuwn.me>
2023-05-06 02:24:42 +0000

    chore: bump to 1.1.4

--- include/soyuz/soyuz.hh
+++ include/soyuz/soyuz.hh
@@ -17,6 +17,6 @@
  * Thanks for bringing this to my attention, @BlueXAyman */
 #define LUNAR_WINDOW_NAME_BASE "Lunar Client 1."
 #define DISCORD_IPC_NAMED_PIPE_NAME L"\\Device\\NamedPipe\\discord-ipc-0"
-#define WINDOW_TRAY_NAME "Soyuz - 1.1.3 | Copyright (C) 2021-2022 Fuwn"
+#define WINDOW_TRAY_NAME "Soyuz - 1.1.4 | Copyright (C) 2021-2023 Fuwn"
 
 #endif // SOYUZ_SOYUZ_HH
